9256d80c272bd02aff6c1cc39bc75c1322f66013
William Kennedy <bill@ardanstudios.com>
2014-12-24 09:05:34 -0500

    Made this code more readable

--- services/buoyService/buoyService.go
+++ services/buoyService/buoyService.go
@@ -6,8 +6,6 @@
 package buoyService
 
 import (
-	"strings"
-
 	"github.com/goinggo/beego-mgo/models/buoyModels"
 	"github.com/goinggo/beego-mgo/services"
 	"github.com/goinggo/beego-mgo/utilities/helper"
@@ -48,14 +46,15 @@ func FindStation(service *services.Service, stationID string) (*buoyModels.BuoyS
 	tracelog.Startedf(service.UserID, "FindStation", "stationID[%s]", stationID)
 
 	var buoyStation buoyModels.BuoyStation
-	if err := service.DBAction(Config.Database, "buoy_stations",
-		func(collection *mgo.Collection) error {
-			queryMap := bson.M{"station_id": stationID}
+	f := func(collection *mgo.Collection) error {
+		queryMap := bson.M{"station_id": stationID}
 
-			tracelog.Trace(service.UserID, "FindStation", "MGO : db.buoy_stations.find(%s).limit(1)", mongo.ToString(queryMap))
-			return collection.Find(queryMap).One(&buoyStation)
-		}); err != nil {
-		if strings.Contains(err.Error(), "not found") == false {
+		tracelog.Trace(service.UserID, "FindStation", "MGO : db.buoy_stations.find(%s).limit(1)", mongo.ToString(queryMap))
+		return collection.Find(queryMap).One(&buoyStation)
+	}
+
+	if err := service.DBAction(Config.Database, "buoy_stations", f); err != nil {
+		if err != mgo.ErrNotFound {
 			tracelog.CompletedError(err, service.UserID, "FindStation")
 			return nil, err
 		}
@@ -70,13 +69,14 @@ func FindRegion(service *services.Service, region string) ([]buoyModels.BuoyStat
 	tracelog.Startedf(service.UserID, "FindRegion", "region[%s]", region)
 
 	var buoyStations []buoyModels.BuoyStation
-	if err := service.DBAction(Config.Database, "buoy_stations",
-		func(collection *mgo.Collection) error {
-			queryMap := bson.M{"region": region}
+	f := func(collection *mgo.Collection) error {
+		queryMap := bson.M{"region": region}
 
-			tracelog.Trace(service.UserID, "FindRegion", "Query : db.buoy_stations.find(%s)", mongo.ToString(queryMap))
-			return collection.Find(queryMap).All(&buoyStations)
-		}); err != nil {
+		tracelog.Trace(service.UserID, "FindRegion", "Query : db.buoy_stations.find(%s)", mongo.ToString(queryMap))
+		return collection.Find(queryMap).All(&buoyStations)
+	}
+
+	if err := service.DBAction(Config.Database, "buoy_stations", f); err != nil {
 		tracelog.CompletedError(err, service.UserID, "FindRegion")
 		return nil, err
 	}
